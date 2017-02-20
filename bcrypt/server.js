b7e7d3dc710496fc73450186e4e3779fed4d4c9c
Dominic Serrano <dmsrojo@gmail.com>
2017-02-20 12:36:35 -0600

    Knex query to check if there is a user already created working. Need to incorporate bcrypt logic now

--- bcrypt/server.js
+++ bcrypt/server.js
@@ -15,19 +15,40 @@ app.use(express.static('public'));
 // A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body). This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Login routes
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.post('/', ({body: {username, password}}, res) => {
-  console.log(username, password);
+  console.log('LOGIN', username, password);
   res.render('index');
 });
 
+// Register routes
 app.get('/register', (req, res) => {
   res.render('register');
 });
 
+app.post('/register', ({body: {username, password}}, res) => {
+  // Check users table to see if there is already a user saved with the same username
+  db('users').where({ 'username': username })
+  .first('username')
+  .then((user) => {
+
+    if (user) {
+      // Send error message if user already exists
+      res.render('register', {err: 'This user already exists.'})
+    } else {
+      // Hash password and create user
+      res.send('Successfully registered. Please log in.');
+    };
+
+
+  })
+  .catch((err) => console.log(err.toString()))
+
+});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
 
