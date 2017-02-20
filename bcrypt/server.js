f3b49fea6552afa189d6d5a84063901e94a12da9
Dominic Serrano <dmsrojo@gmail.com>
2017-02-20 11:35:44 -0600

    Fixed whitespace error

--- bcrypt/server.js
+++ bcrypt/server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// Connect to the database
+const environment = process.env.NODE_ENV || 'development';
+// Select development config setting
+const configuration = require('./db/config.js')[environment];
+const db = require('knex')(configuration);
+
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 // A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body). This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
