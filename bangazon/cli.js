223d2ab0a53a0fa760164a23ae520a8e8e616cee
Dominic Serrano <dmsrojo@gmail.com>
2017-03-13 09:56:20 -0500

    Can now respond to each command for bangazon

--- bangazon/cli.js
+++ bangazon/cli.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const readline = require('readline');
+const { startMenu } = require('./menuOptions.js')
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,22 +10,41 @@ const rl = readline.createInterface({
 });
 
 
-// Call prompt
+// Display menu and call prompt
+startMenu();
 rl.prompt();
 
+
 rl.on('line', (line) => {
-  switch(line.trim()) {
-    case 'hello':
-      console.log('world!');
+  switch(parseInt(line)) {
+    case 1:
+      console.log('1');
+      break;
+    case 2:
+      console.log('2');
+      break;
+    case 3:
+      console.log('3');
+      break;
+    case 4:
+      console.log('4');
+      break;
+    case 5:
+      console.log('5');
+      break;
+    case 6:
+      console.log('6');
+      break;
+    case 7:
+      console.log('\nGoodbye!');
+      process.exit();
       break;
     default:
       console.log(`Say what? I might have heard '${line.trim()}'`);
       break;
   }
 
+  // Create new prompt after each
   rl.prompt();
 
-}).on('close', () => {
-  console.log('Have a great day!');
-  process.exit(0);
 });
--- bangazon/menuOptions.js
+++ bangazon/menuOptions.js
@@ -1,5 +1,18 @@
 'use strict';
 
 const startMenu = () => {
-
+  console.log(`
+    *********************************************************
+    **  Welcome to Bangazon! Command Line Ordering System  **
+    *********************************************************
+    1. Create a customer account
+    2. Choose active customer
+    3. Create a payment option
+    4. Add product to shopping cart
+    5. Complete an order
+    6. See product popularity
+    7. Leave Bangazon!
+  `)
 };
+
+module.exports = { startMenu };
