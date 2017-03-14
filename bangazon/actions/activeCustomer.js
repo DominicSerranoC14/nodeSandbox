16731fb234d012d615ffd2dd99e2b798affe5b13
Dominic Serrano <dmsrojo@gmail.com>
2017-03-14 16:14:51 -0500

    Displaying all products. Working on persisting an active customer.

--- bangazon/actions/activeCustomer.js
+++ bangazon/actions/activeCustomer.js
@@ -6,9 +6,9 @@ const prompt = require('prompt');
 // Query the database and display all users
 const showActiveCustomers = () => {
   console.log('Which customer will be active?');
-  DB.each(`select customer_id, name from customers`, (err, {customer_id, name}) => {
+  DB.each(`select customerId, name from customers`, (err, {customerId, name}) => {
     errHandler(err);
-    console.log(`${customer_id}. ${name}`);
+    console.log(`${customerId}. ${name}`);
   }, captureActiveListener);
 };
 
@@ -16,7 +16,7 @@ const showActiveCustomers = () => {
 const captureActiveListener = () => {
   // Capture input from user and query the db to get the user
   prompt.get('$', (err, { $ }) => {
-    DB.get(`select name from customers where customer_id = ${parseInt($)}`, (err, { name }) => {
+    DB.get(`select name from customers where customerId = ${parseInt($)}`, (err, { name }) => {
       console.log(`\nWelcome ${name}!\n`);
       // Require in startMenu method here to avoid circular dep
       setTimeout(require('./menuOptions.js').startMenu, 2000);
