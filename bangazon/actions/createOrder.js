d0857606e9c5a8013a1e1d31b986e81036c9665c
Dominic Serrano <dmsrojo@gmail.com>
2017-03-16 11:25:31 -0500

    createOrder and determineIfUnpaid are working as standalone function. Integrating into app now

--- bangazon/actions/createOrder.js
+++ bangazon/actions/createOrder.js
@@ -2,17 +2,45 @@
 
 const { DB, errHandler } = require('../db.js');
 
-process.env.CURRENT_USER_ID = 1;
+let userId = process.env.CURRENT_USER_ID;
 
-const createOrder = () => {
 
-  // Insert a new order with the current users id
-  // and a payment value of false (not paid)
-  DB.run(`insert into orders values (
-    null, ${process.env.CURRENT_USER_ID},
-    null, -1
-  )`)
+const determineIfUnpaidOrder = () => {
+  // Check to make sure there is an active user
+  if (!userId) {
+    return console.log('\nPlease create or choose an active customer.\n');
+  };
+
+  // Query to find unpaid orders for current customer
+  DB.get(`select * from orders o
+    where
+    o.customerId = ${userId}
+    and
+    o.paymentStatus = -1
+  `, (err, resultObj) => {
+    errHandler(err);
+
+    // If there is an unpaid order, return that order obj
+    if (resultObj) {
+      return resultObj;
+    };
+
+    // If all orders are paid, create a new order for the customer
+    createOrder();
+  });
 
 };
 
-createOrder();
+
+// Creates a new order for the acitve customer
+const createOrder = () => {
+    // Insert a new order with the current users id
+    // and a payment value of false (not paid)
+    DB.run(`insert into orders values (
+      null, ${process.env.CURRENT_USER_ID},
+      null, -1
+    )`);
+};
+
+
+module.exports = determineIfUnpaidOrder;
